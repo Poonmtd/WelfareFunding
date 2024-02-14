486a8bd98ffe92d3b3dce0a43691250d78394b41
monthida <poonmonthida@gmail.com>
2024-02-14 15:18:36 +0700

    update

--- welfarefunding/model/WelfareCondition.py
+++ welfarefunding/model/WelfareCondition.py
@@ -5,21 +5,42 @@ from xerial.IntegerColumn import IntegerColumn
 from xerial.Children import Children
 
 from xerial.input.RichTextInput import RichTextInput
+from xerial.input.TextInput import TextInput
 from xerial.input.ReferenceRadioInput import ReferenceSelectInput
 
 class WelfareCondition(Record):
-	welfareType = IntegerColumn(
-		default=-1,
-		foreignKey="WelfareType.id",
-		input=ReferenceSelectInput(
-			label="ประเภทสวัสดิการ",
-			url="welfarefunding/welfaretype/option/get",
-			order="1.0",
+	welfareName = StringColumn(
+		isRepresentative=True,
+		length=100,
+		input=TextInput(
+			label="ชื่อสวัสดิการ",
 			isRequired=True,
 			isTable=True,
-			isSearch=True
+			order="1.0"
 		)
 	)
+
+	# welfareDescription = StringColumn(
+	# 	length=200,
+	# 	input=TextInput(
+	# 		label="รายละเอียดของสวัสดิการ",
+	# 		isRequired=True,
+	# 		isTable=True,
+	# 		order="2.0"
+	# 	)
+	# )
+	# welfareType = IntegerColumn(
+	# 	default=-1,
+	# 	foreignKey="WelfareType.id",
+	# 	input=ReferenceSelectInput(
+	# 		label="ประเภทสวัสดิการ",
+	# 		url="welfarefunding/welfaretype/option/get",
+	# 		order="1.0",
+	# 		isRequired=True,
+	# 		isTable=True,
+	# 		isSearch=True
+	# 	)
+	# )
 
 	# welfareCondition = StringColumn(
 	# 	isRepresentative=True,
