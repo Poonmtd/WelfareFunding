359d8d922a2f07c4383cfd0c2a4d7d38626c6c0b
ntnnw <gibnattanit@gmail.com>
2024-03-21 00:09:00 +0700

    update

---
+++ welfarefunding/model/Nametitle.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+
+class Nametitle(IntEnum):
+    MR	 = 0
+    MRS  = 1
+    MISS = 2
+    BOY = 3
+    GIRL = 4
+    MONK = 5
+    
+Nametitle.label = {
+    Nametitle.MR : "นาย",
+    Nametitle.MRS : "นาง",
+    Nametitle.MISS : "นางสาว",
+    Nametitle.BOY : "เด็กชาย",
+    Nametitle.GIRL : "เด็กหญิง",
+    Nametitle.MISS : "พระ"
+}
